487fc3aa6a7fe50737e40f3fc4682a170a83fd45
goreleaserbot <goreleaser@carlosbecker.com>
2024-03-13 03:40:25 +0900

    Brew formula update for kcg version v0.13.2

--- kcg.rb
+++ kcg.rb
@@ -5,20 +5,20 @@
 class Kcg < Formula
   desc "kumak1 Convenient Git tools."
   homepage "https://github.com/kumak1/kcg"
-  version "0.13.1"
+  version "0.13.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kumak1/kcg/releases/download/v0.13.1/kcg_0.13.1_darwin_arm64.tar.gz"
-      sha256 "c7a106e2664e3ea025974506f5b674e0effb5ec29f302628be6f5b6d99c5e7af"
+      url "https://github.com/kumak1/kcg/releases/download/v0.13.2/kcg_0.13.2_darwin_arm64.tar.gz"
+      sha256 "7d3c5820d92a9296fbb243432f405827044cdbfa417670716ffd3ab236e0c656"
 
       def install
         bin.install Dir['kcg']
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kumak1/kcg/releases/download/v0.13.1/kcg_0.13.1_darwin_amd64.tar.gz"
-      sha256 "0942302efcceeb544030ed373d0a7034af4a313db086b5d161a556ce969d519f"
+      url "https://github.com/kumak1/kcg/releases/download/v0.13.2/kcg_0.13.2_darwin_amd64.tar.gz"
+      sha256 "22f381721850f8ffed6b91c34f308523e8e33992dbd8f009fd1200ebb1771487"
 
       def install
         bin.install Dir['kcg']
@@ -28,16 +28,16 @@ class Kcg < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kumak1/kcg/releases/download/v0.13.1/kcg_0.13.1_linux_arm64.tar.gz"
-      sha256 "dcdf7317927bdca6dd64c66a6b5632a751509689c7dd71a6482f255a24f32123"
+      url "https://github.com/kumak1/kcg/releases/download/v0.13.2/kcg_0.13.2_linux_arm64.tar.gz"
+      sha256 "31fbb8f15205d4c1a8a7586030f087645805fe0c9ad685fc73cf0fd603079c9c"
 
       def install
         bin.install Dir['kcg']
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kumak1/kcg/releases/download/v0.13.1/kcg_0.13.1_linux_amd64.tar.gz"
-      sha256 "707011fbf647fd19760dd9da0698406df3e4b225fed49a4c0fcafd9ec9de86ca"
+      url "https://github.com/kumak1/kcg/releases/download/v0.13.2/kcg_0.13.2_linux_amd64.tar.gz"
+      sha256 "8ee3e0b006fab8cc1eea4679e6434990530a80f9e5731ee7759ff5b0c3916f36"
 
       def install
         bin.install Dir['kcg']
